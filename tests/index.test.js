df91a255cf07c0ab3354aff1d470d4bcd0b6531a
ChrisCodesThings <153457755+ChrisCodesThings@users.noreply.github.com>
2024-04-26 17:19:20 +0100

    tests: remove unncessary tests and simplify remaining

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,39 +1,16 @@
 
 const { default: testFunc } = await import("../");
 
-describe("check for bad input", () => {
-    test("nothing", async () => {
-        expect(testFunc()).toBeUndefined();
-    });
-
-    test("boolean", async () => {
-        expect(testFunc(true)).toBeUndefined();
-    });
-
-    test("a number", async () => {
-        expect(testFunc(123)).toBeUndefined();
-    });
-
-    test("not a css colour string", async () => {
-        expect(testFunc("foo")).toBeUndefined();
-    });
-});
-
 describe("compress some colours", () => {
     test("should not compress", async () => {
         expect(testFunc("#123456")).toEqual("#123456");
-    });
-
-    test("should not compress", async () => {
         expect(testFunc("#c0ffeeee")).toEqual("#c0ffeeee");
-    });
-
-    test("should not compress", async () => {
         expect(testFunc("#f00d")).toEqual("#f00d");
     });
 
     test("should remove alpha", async () => {
         expect(testFunc("#f00f")).toEqual("#f00");
+        expect(testFunc("#6495edff")).toEqual("#6495ed");
     });
 
     test("should compress", async () => {
@@ -42,9 +19,5 @@ describe("compress some colours", () => {
 
     test("should compress with alpha removed", async () => {
         expect(testFunc("#aabbccff")).toEqual("#abc");
-    });
-
-    test("should remove alpha", async () => {
-        expect(testFunc("#6495edff")).toEqual("#6495ed");
     });
 });
